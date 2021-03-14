59adb8f05b37280fd9afd543b6bb55f88ce53053
Lucas Maniero <lucasfomaniero@gmail.com>
2021-03-14 15:26:30 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const htmlPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
+    plugins: [
+        new htmlPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.jsx$/,
+              exclude: /node_modules/,
+              use: 'babel-loader',
+            }
+        ]
+    }
+}
